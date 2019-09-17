e0c7b4247fa7037fac28d45e58d1f80acf81be22
Maggie <maggieholbl@gmail.com>
2019-09-17 00:53:58 +0000

    add queries using select

---
+++ 2_queries_joins/1_total_assignment_duration.sql
@@ -0,0 +1,3 @@
+SELECT sum(duration) AS total_duration
+FROM students JOIN assignment_submissions ON student_id = students.id
+WHERE students.name = 'Ibrahim Schimmel';
---
+++ 2_queries_joins/2_cohort_total_assignment_duration.sql
@@ -0,0 +1,3 @@
+SELECT sum(duration) AS total_duration
+FROM students JOIN assignment_submissions ON student_id = students.id JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12';
